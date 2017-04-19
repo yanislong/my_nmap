617292d4090701f8083595fd8e1045769f06b68f
yanislong <335916781@qq.com>
2017-04-19 11:26:21 +0100

    update file proxy

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
 
+import json
 import time
 import sys
 import requests, bs4, re
 
-def access_pro():
-    url = "http://www.xicidaili.com/nn/1"
+def access_pro(num=1):
+    url = "http://www.xicidaili.com/nn/" + str(num)
+    ll = []
+    data = {}
     header = {}
     header['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
     header['Referer'] = ""
     r = requests.get(url, headers=header)
     html = bs4.BeautifulSoup(r.content)
     tt = html.find('table').find_all('tr')
-    n = 0
-    ll = []
     for i in tt:
         for j in i.contents:
             if type(j) != bs4.element.NavigableString:
-#                print j.string
                 ll.append(j.string)
-        print '*****************'
-        n += 1
-#    print ll
-    for l in range(11,10000,10):
+    for l in range(11,1000,10):
         try:
-            print ll[l]
-            print ll[l+1]
-            print ll[l+4]
+            ip = []
+#            print ll[l]
+#            print ll[l+1]
+#            print ll[l+4]
+            ip.append(ll[l])
+            ip.append(ll[l+1])
+            ip.append(ll[l+4])
+            for i in range(400):
+                data[i] = ip
         except:
             print "quit"
-            sys.exit()
-#    for k in ll:
-#        print k
+#            sys.exit()
+        finally:
+            with open('data.txt' + str(num),'w') as ff:
+                json.dump(data,ff)
 
-access_pro()
+for i in range(1,10):
+    print i
+    access_pro(i)
---
+++ proxy/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+import json
+
+with open('data.txt','r') as f:
+    print f.read()
