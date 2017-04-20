6aed012e7ebbe1f197483206069c9b6dbc846df8
yanislong <335916781@qq.com>
2017-04-20 11:31:50 +0100

    modify proxy

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -7,38 +7,58 @@ import sys
 import requests, bs4, re
 
 def access_pro(num=1):
-    url = "http://www.xicidaili.com/nn/" + str(num)
     ll = []
-    data = {}
-    header = {}
-    header['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
-    header['Referer'] = ""
-    r = requests.get(url, headers=header)
-    html = bs4.BeautifulSoup(r.content)
-    tt = html.find('table').find_all('tr')
-    for i in tt:
-        for j in i.contents:
-            if type(j) != bs4.element.NavigableString:
-                ll.append(j.string)
     n = 0
-    for l in range(11,1000,10):
+#    proxie = {'http':'http://61.186.164.98:8080'}
+    for ii in range(1,10):
+        url = "http://www.xicidaili.com/nn/" + str(ii)
+        data = {}
+        header = {}
+        header['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+        header['Referer'] = ""
+        r = requests.get(url, headers=header, timeout=10)
+        html = bs4.BeautifulSoup(r.content)
+        tt = html.find('table').find_all('tr')
+        for i in tt:
+            for j in i.contents:
+                jia = []
+                if type(j) == bs4.element.NavigableString:
+                    pass
+#                    print "NavigableString..."
+                elif type(j) == bs4.element.Tag:
+#                    print j.string
+                    if j.string:
+                        jia.append(j.string)
+                    for y in j.contents:
+                        if y.name == 'a':
+                            jia.append(y.string)
+#                    print '>>>>>>>>>>>>>>' +str(jia)+'<<<<<<<<<<<<<<'
+                ll.append(jia)
+
+    for i in range(0):
+        if ll[i]:
+            print ll[i][0],i
+
+    for l in range(24,len(ll),21):
         try:
             ip = []
 #            print ll[l]
-#            print ll[l+1]
+#            print ll[l+2]
 #            print ll[l+4]
-            ip.append(ll[l])
-            ip.append(ll[l+1])
-            ip.append(ll[l+4])
-            data[n] = ip
-            n += 1
+#            print ll[l+8]
+            if ll[l]:
+                ip.append(ll[l])
+                ip.append(ll[l+2])
+                ip.append(ll[l+4])
+                ip.append(ll[l+8])
+                data[n] = ip
+                n += 1
         except:
             print "quit"
 #            sys.exit()
         finally:
-            with open('data.txt' + str(num),'w') as ff:
+            with open('data.txt','w') as ff:
                 json.dump(data,ff)
 
-for i in range(1,4):
-    print i
+for i in range(1):
     access_pro(i)
--- proxy/test.py
+++ proxy/test.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import json
+import bs4
 import requests
 
-with open('data.txt1','r') as f:
+with open('data.txt','r') as f:
     ip = json.load(f)
 
 url = "http://dev.www.xueguoxue.com"
 header = {}
-for i in range(10):
-    i = str(i)
-    print ip[i][0]
-    print ip[i][1]
-    proxie = {'http':'http://' + ip[i][0] + ":" + ip[i][1]}
-    print proxie
-    try:
-        r = requests.get(url, proxies=proxie, timeout=10)
-        print r.content
-        print "**********************************************************"
+data = {}
+
+print 'shell start ....'
+for i in range(1,905):
+    while True:
+        i = str(i)
+        proxie = {'http': ip[i][3][0] +'://' + ip[i][0][0] + ":" + ip[i][1][0]}
+        city = ip[i][2][0]
+#        print proxie
+        try:
+            r = requests.get(url, proxies=proxie, timeout=5)
+        except:
+            print "tiaoguo"
+            break
+#        print "**********************************************************"
         if r.status_code == 200:
-            with open('keyong.txt','a') as ff:
-                json.dump(proxie,ff)
-    except:
-        print "tiaoguo"
+            tt = bs4.BeautifulSoup(r.content)
+            try: 
+                print tt.title.string
+            except:
+                break
+            if tt.title.string == "学国学网 | 专注优秀传统文化教育":
+                ip_c = []
+                ip_c.append(proxie)
+                ip_c.append(city)
+                data[i] = ip_c
+                print data[i]
+with open('keyong.txt','a') as ff:
+    json.dump(data,ff)
+print ">>>>> shell stop"
