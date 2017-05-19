98ca9419a511b57f8179f473efb726380c48f4f7
yanislong <335916781@qq.com>
2017-05-19 11:25:41 +0100

    update file

--- appium/test.py
+++ appium/test.py
@@ -17,13 +17,34 @@ desired_caps['platformVersion'] = '5.1.2'
 desired_caps['deviceName'] = 'Lenovo P1c72'
 
 #desired_caps['app'] = PATH('C:\Users\huang\Downloads\com.aixuetang.online_2.2.0_163.apk') 
-desired_caps['app'] = r'C:\Users\huang\Downloads\app-release.apk'
-#desired_caps['appPackage'] = 'com.aixuetang.online'
-#desired_caps['appActivity'] = 'com.aixuetang.mobile.activities.HomeActivity'
+#desired_caps['app'] = r'C:\Users\huang\Downloads\app-release.apk'
+
 #如果设置的是app在电脑上的路径，则不需要配appPackage和appActivity，同理反之
 
+#爱学堂
+#desired_caps['appPackage'] = 'com.aixuetang.online'
+#desired_caps['appActivity'] = 'com.aixuetang.mobile.activities.HomeActivity'
+#微信
+#desired_caps['appPackage'] = 'com.tencent.mm'
+#desired_caps['appActivity'] = '.ui.LauncherUI'
+
+#系统
+#desired_caps['appPackage'] = "com.meizu.flyme.launcher"
+#desired_caps['appActivity'] = ".Launcher"
+
+desired_caps['appPackage'] = 'com.android.settings'
+desired_caps['appActivity'] = '.Settings'
+
 dd = webdriver.Remote('http://172.16.20.92:4723/wd/hub', desired_caps)
-time.sleep(10)#启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然下面会一直报错定位不到元素
+time.sleep(5)#启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然下面会一直报错定位不到元素
+
+dd.find_element_by_name(u'双卡和网络').click()
+dd.find_element_by_id('com.android.settings:id/switchWidget').click()
+time.sleep(1)
+
+dd.quit()
+sys.exit()
+
 width = dd.get_window_size()['width']
 height = dd.get_window_size()['height']
 for i in range(2):
--- appium/xgx_app.py
+++ appium/xgx_app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+import random
 import sys
 import os
 import time
@@ -14,20 +15,21 @@ def login(name,passwd):
         dd.find_element_by_name(u'手机号').send_keys(name)
     except:
         dd.find_element_by_name(name).send_keys(name)
-    time.sleep(1)
+    time.sleep(0.5)
     try:
         dd.find_element_by_name(u'密码').send_keys(passwd)
     except:
         dd.find_element_by_name(passwd).send_keys(name)
     #dd.hide_keyboard()
-    time.sleep(1)
+    time.sleep(0.5)
     dd.find_element_by_name(u'登录').click()
-    time.sleep(1)
+    time.sleep(0.5)
+    num = str(random.random())
+    dd.get_screenshot_as_file(r'/root/Desktop/app_tu/xueguoxue' + num +'.png')
     content2 = dd.find_element_by_name(name).get_attribute('text')
     content3 = dd.find_element_by_name(passwd).get_attribute('text')
     print content2
     print content3
-#    dd.keyevent(123)
     for i in range(0,len(content3)):
         dd.keyevent(67)
     dd.find_element_by_name(content2).click()
@@ -48,17 +50,27 @@ def regiest():
     time.sleep(2)
 
 if __name__ == "__main__":
-    desired_caps = {}
-    desired_caps['platformName'] = 'Android'
-    desired_caps['platformVersion'] = '5.1.2'
-    desired_caps['deviceName'] = 'Lenovo P1c72'
-    desired_caps['unicodeKeyboard'] = True
-    desired_caps['resetKeyboard'] = True
-    desired_caps['app'] = r'C:\Users\huang\Downloads\app-release.apk'
-    dd = webdriver.Remote('http://172.16.20.92:4723/wd/hub', desired_caps)
-    time.sleep(5)#启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然下面>会一直报错定位不到元素
-    parme = [['long','123123'],['gege','000000'],['13100000000','xxxxxx']]
-    for i in parme:
-        login(i[0],i[1])
-    dd.quit()
-    print '>>quit!'
+    try:
+        parme = [['long','123123'],['gege','000000'],['13100000000','xxxxxx']]
+        for i in range(1):#parme:
+            desired_caps = {}
+            desired_caps['platformName'] = 'Android'
+            desired_caps['platformVersion'] = '5.1.2'
+            desired_caps['deviceName'] = 'Lenovo P1c72'
+            desired_caps['unicodeKeyboard'] = True
+            desired_caps['resetKeyboard'] = True
+            desired_caps['appPackage'] = 'com.xueguoxue'
+            desired_caps['appActivity'] = '.MainActivity'
+#            desired_caps['app'] = r'C:\Users\huang\Downloads\app-release.apk'
+            dd = webdriver.Remote('http://172.16.20.92:4723/wd/hub', desired_caps)
+            time.sleep(1)
+#            dd.get_screenshot_as_file(r'/root/Desktop/hello.png')
+            time.sleep(5)#启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然>下面>会一直报错定位不到元素
+            forget_passwd()
+#            login(i[0],i[1])
+    except:
+        print 'error quit'
+        dd.quit()
+    finally:
+        print '>>quit!'
+        dd.quit()
